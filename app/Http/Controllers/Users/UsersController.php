362287c0d459fc0679499bc04875807e698441d9
Kikiriki070204 <kiarabarrientossalazar@gmail.com>
2024-06-01 11:17:37 -0600

    pt2 del leftJoin

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -215,7 +215,7 @@ class UsersController extends Controller
 
         $users = DB::table('usuarios')
             ->join('roles', 'usuarios.rol_id', '=', 'roles.id')
-            ->join('departamentos', 'usuarios.departamento_id', '=', 'departamentos.id')
+            ->leftJoin('departamentos', 'usuarios.departamento_id', '=', 'departamentos.id')
             ->join('areas', 'usuarios.area_id', '=', 'areas.id')
             ->leftJoin('locaciones', 'usuarios.locacion_id', '=', 'locaciones.id')
             ->select(
