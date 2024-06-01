41b0c9a573b3ff8fdbf829bf31d17550e888982d
Kikiriki070204 <kiarabarrientossalazar@gmail.com>
2024-06-01 12:51:51 -0600

    show

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -112,7 +112,7 @@ class UsersController extends Controller
     {
         $user = DB::table('usuarios')
             ->join('roles', 'usuarios.rol_id', '=', 'roles.id')
-            ->join('departamentos', 'usuarios.departamento_id', '=', 'departamentos.id')
+            ->leftJoin('departamentos', 'usuarios.departamento_id', '=', 'departamentos.id')
             ->join('areas', 'usuarios.area_id', '=', 'areas.id')
             ->leftJoin('locaciones', 'usuarios.locacion_id', '=', 'locaciones.id')
             ->select(
