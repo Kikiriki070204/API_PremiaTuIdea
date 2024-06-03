c763e4fea2382064753cde21896942e9bda00573
Kevin-Burciiaga <145623464+Kevin-Burciiaga@users.noreply.github.com>
2024-06-03 16:50:21 -0600

    Seeder de usuario final(corregido ya si)

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,8 @@ use App\Http\Controllers\Users\UsersController;
 use App\Http\Controllers\EstadoUsuarioPremiosController;
 use App\Http\Controllers\UsuarioPremiosController;
 use Symfony\Component\CssSelector\Node\FunctionNode;
+use Illuminate\Support\Facades\File;
+use Illuminate\Http\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,6 +34,25 @@ use Symfony\Component\CssSelector\Node\FunctionNode;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/ideas/{filename}', function ($filename)
+{
+    $path = storage_path('app/ideas/' . $filename);
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+$response = Response::make($file, 200);
+$response->header("Content-Type", $type);
+    $response->header("Content-Type", $type);
+
+    return $response;
 });
 
 //Rutas de autenticación y registro
